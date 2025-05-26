07f8282cd400658e7f2da9305da73512aef6f32b
maialoha <maialoha426@gmail.com>
2025-05-26 10:38:14 +0900

    kadai_010をアップロード

---
+++ kadai_010/holiday.js
@@ -0,0 +1,32 @@
+const holidays = [
+  "元日",
+  "成人の日",
+  "建国記念の日",
+  "春分の日",
+  "昭和の日",
+  "憲法記念日",
+  "みどりの日",
+  "こどもの日",
+  "海の日",
+  "山の日",
+  "敬老の日",
+  "秋分の日",
+  "体育の日",
+  "文化の日",
+  "勤労感謝の日",
+  "天皇誕生日"
+];
+
+// for文を使った出力
+console.log("【for文での出力】");
+for (let i = 0; i < holidays.length; i++) {
+  console.log(holidays[i]);
+}
+
+// while文を使った出力
+console.log("【while文での出力】");
+let j = 0;
+while (j < holidays.length) {
+  console.log(holidays[j]);
+  j++;
+}
